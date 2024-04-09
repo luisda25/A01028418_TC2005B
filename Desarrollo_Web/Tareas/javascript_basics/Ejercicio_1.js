6f75476bbd868638a5ce2f69d313c81bad65c318
Luis Daniel <luisdaf04@outlook.com>
2024-04-08 20:01:34 -0600

    Added ejercicios 1 y 2 js

---
+++ Desarrollo_Web/Tareas/javascript_basics/Ejercicio_1.js
@@ -0,0 +1,26 @@
+function no_repeat(cadena) {
+    const count = {};
+
+    for (let char of cadena) {
+        if (count[char]) {
+            count[char]++;
+        } else {
+            count[char] = 1;
+        }
+    }
+
+    for (let char of cadena) {
+        if (count[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
+}
+
+const cadena1 = 'abacddbec';
+console.log(no_repeat(cadena1));
+const cadena2 = 'aonfourhv';
+console.log(no_repeat(cadena2));
+const cadena3 = 'fjeiofahejñaf';
+console.log(no_repeat(cadena3));
---
+++ Desarrollo_Web/Tareas/javascript_basics/Ejercicio_2.js
@@ -0,0 +1,23 @@
+function bubbleSort(lista) {
+    const n = lista.length;
+    let flag;
+
+    do {
+        flag = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (lista[i] > lista[i + 1]) {
+                [lista[i], lista[i + 1]] = [lista[i + 1], lista[i]];
+                flag = true;
+            }
+        }
+    } while (flag);
+
+    return lista;
+}
+
+const lista1 = [5, 3, 8, 1, 2];
+console.log(bubbleSort(lista1));
+const lista2 = [10, 9, 11, 15, 8, 2];
+console.log(bubbleSort(lista2));
+const lista3 = [9, 5, 3, 1, 4, 10];
+console.log(bubbleSort(lista3));
